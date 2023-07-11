ba31d7d61dcbc67f4f1c9e9a075a40552ea4934f
JenniferEinstein <jennifereinstein@gmail.com>
2023-07-10 20:24:41 -0400

    Not even sure what changes I made from last time.

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const PORT = 3003;
 //ROUTES
 app.get("/", (request, response) => {
     response.send("Home Sweet Home " 
-    + "<a href='/hillel'>Hillel the Elder</a> " 
+    + "<a href='terminator'>The Terminator</a>"
+    + " " + "<a href='/magic8'>Magic 8 Ball</a> "  
+    + " " + "<a href='/hillel'>Hillel the Elder</a> "
     + " " + "<a href='/senesh'>Hannah Senesh</a> "
     + " " + "<a href='/pavelfriedman'>The Butterfly</a> "
     + " " + "<a href='/milton'>John Milton</a> "
@@ -21,9 +23,24 @@ app.get("/", (request, response) => {
     )
 });
 
+app.get("/terminator", (request, response)=> {
+    response.send("I'll be back")
+    response.send("Hasta la vista, Baby.")
+})
+
+
+app.get("/magic8", (request, response) => {
+    response.send("Outlook not so good")
+});
+
+
+
+
 app.get("/hillel", (request, response) => {
     response.send("If not now, when?")
 });
+
+
 
 app.get("/senesh", (request, response) => {
     response.send("Blessed is the match consumed in kindling flame.")
@@ -58,3 +75,33 @@ app.get("/snowflake", (request, response) => {
 });
 
 
+const magic8Responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - Definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes, and signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+  ]
+
+randomAnswer = () => {
+    let randomIndex = Math.floor(Math.random() * magic8Responses.length)
+    return magic8Responses[randomIndex]
+}
+
+
+// EXPORT 
+module.exports = app
